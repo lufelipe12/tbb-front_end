0bd4e1c2ee1267c7b8eb304a2bfa10c9ad705dcb
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-07-24 15:45:17 -0300

    feat: added dropdown menu

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,5 @@
 import { VscSearch } from "react-icons/vsc"
+import { IoIosArrowDown } from "react-icons/io"
 
 import header_img from "../../assets/header-img-2.png"
 import { StyledHeader, StyledNav, StyledDiv } from "./styles"
@@ -13,23 +14,89 @@ const Header = () => {
             <div className="menu-button"></div>
           </label>
           <ul className="menu">
-            <li>Brand philosophy</li>
-            <li>Product technology</li>
-            <li>All products</li>
-            {/* <li>Intibiome wellness</li>
-            <li>Intibiome active</li>
-            <li>Intibiome agecare</li>
-            <li>Article 1</li>
-            <li>Article 2</li>
-            <li>Article 3</li>
-            <li>Faq</li>
-            <li>Contact us</li> */}
+            <li>
+              <a href="#">brand philosophy</a>
+            </li>
+            <li>
+              <a href="#">product technology</a>
+            </li>
+            <li>
+              <a href="#">all products</a>
+            </li>
+            <li>
+              <a href="#">intibiome wellness</a>
+            </li>
+            <li>
+              <a href="#">intibiome active</a>
+            </li>
+            <li>
+              <a href="#">intibiome agecare</a>
+            </li>
+            <li>
+              <a href="#">article 1</a>
+            </li>
+            <li>
+              <a href="#">article 2</a>
+            </li>
+            <li>
+              <a href="#">article 3</a>
+            </li>
+            <li>
+              <a href="#">faq</a>
+            </li>
+            <li>
+              <a href="#">contact us</a>
+            </li>
+          </ul>
+          <ul className="menu-desktop">
+            <li>
+              <div className="dropdown">
+                <button className="dropbtn">
+                  about us
+                  <IoIosArrowDown className="arrow" />{" "}
+                </button>
+                <div className="dropdown-content">
+                  <a href="#">brand philosophy</a>
+                  <a href="#">product technology</a>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="dropdown">
+                <button className="dropbtn">
+                  our products
+                  <IoIosArrowDown className="arrow" />{" "}
+                </button>
+                <div className="dropdown-content">
+                  <a href="#">all products</a>
+                  <a href="#">intibiome wellness</a>
+                  <a href="#">intibiome active</a>
+                  <a href="#">intibiome agecare</a>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="dropdown">
+                <button className="dropbtn">
+                  intimate health <IoIosArrowDown className="arrow" />{" "}
+                </button>
+                <div className="dropdown-content">
+                  <a href="#">article 1</a>
+                  <a href="#">article 2</a>
+                  <a href="#">article 3</a>
+                  <a href="#">faq</a>
+                </div>
+              </div>
+            </li>
+            <li>
+              <a href="#">contact us</a>
+            </li>
           </ul>
         </section>
       </StyledNav>
       <StyledDiv>
         <img src={header_img} />
-        <VscSearch />
+        <VscSearch className="icon" />
       </StyledDiv>
     </StyledHeader>
   )
